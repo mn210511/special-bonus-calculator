405c7f8a64488133cbbcf26d0693312bf86d7f66
mn210511 <nicolas.herold@outlook.at>
2021-11-01 18:18:49 +0100

    renamed fxml

--- src/main/java/com/mn210511/specialbonuscalculator/HelloController.java
+++ src/main/java/com/mn210511/specialbonuscalculator/HelloController.java
@@ -1,11 +1,20 @@
 package com.mn210511.specialbonuscalculator;
 
+import com.mn210511.specialbonuscalculator.services.Calculator;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+
+
+    public void initialize() {
+        Calculator calculator = new Calculator();
+
+
+    }
+
 
     @FXML
     protected void onHelloButtonClick() {
