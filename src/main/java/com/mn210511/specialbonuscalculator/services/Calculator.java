c38716a2f7654ecd4b7b36048c36aed897436ee2
mn210511 <nicolas.herold@outlook.at>
2021-11-01 18:10:39 +0100

    add comment
    add comment to the methods in Calculator class.

--- src/main/java/com/mn210511/specialbonuscalculator/services/Calculator.java
+++ src/main/java/com/mn210511/specialbonuscalculator/services/Calculator.java
@@ -5,10 +5,10 @@ import java.util.Arrays;
 public class Calculator {
 
     /**
-     *
-     * @param hours
-     * @param days
-     * @return
+     *calculatates the average hours
+     * @param hours the hours that where worked per week
+     * @param days the duration of the worked in the time-model
+     * @return the avg value
      */
     public double averageHours (double hours, int days, boolean shiftYear) {
     int yearInDays = 0;
@@ -21,12 +21,23 @@ public class Calculator {
    return (days/yearInDays)*hours;
 }
 
-public double sumAverageHours (double ... values) {
+    /**
+     * sums all the avg values
+     * @param values the avg values to sum
+     * @return a sum fo the values
+     */
+    public double sumAverageHours (double ... values) {
     return Arrays.stream(values).sum();
 }
 
-
-public double calculateBonus (double salary, double avgHours, double workDaysPerWeek) {
+    /**
+     * calculates the correct bonus to pay
+     * @param salary the salary of the employee
+     * @param avgHours the sum of the avgHours
+     * @param workDaysPerWeek the worktime model (40, 38, 38.5)
+     * @return the bonus to pay
+     */
+    public double calculateBonus (double salary, double avgHours, double workDaysPerWeek) {
 
         return (salary/workDaysPerWeek)*avgHours;
 }
