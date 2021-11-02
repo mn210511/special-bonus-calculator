d62ae7199325b0f4cd8b75a4a606ac9bf0edfe81
mn210511 <nicolas.herold@outlook.at>
2021-11-02 17:04:45 +0100

    add attributes for the controlls

--- src/main/java/com/mn210511/specialbonuscalculator/BonusCalculatorController.java
+++ src/main/java/com/mn210511/specialbonuscalculator/BonusCalculatorController.java
@@ -3,14 +3,42 @@ package com.mn210511.specialbonuscalculator;
 import com.mn210511.specialbonuscalculator.services.Calculator;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
-
-
+import javafx.scene.control.*;
 
 
 public class BonusCalculatorController {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private Button btnAddFields;
+
+    @FXML
+    private CheckBox cbShiftYear;
+
+    @FXML
+    private ComboBox<Boolean> cmbWorkModell;
+
+    @FXML
+    private Label lblBonus;
+
+    @FXML
+    private TextField txtDays1;
+
+    @FXML
+    private TextField txtDays2;
+
+    @FXML
+    private TextField txtHours1;
+
+    @FXML
+    private TextField txtHours2;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtSalary;
 
     private Node[] hourFields = new Node[12];
     private Node[] dayFields = new Node[12];
