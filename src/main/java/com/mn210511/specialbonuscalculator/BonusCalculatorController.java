39bce1f590186c2c092f46f3429bb65967bff013
mn210511 <nicolas.herold@outlook.at>
2021-11-02 17:00:46 +0100

    small change to fxml and controller
    add arrays that store the dynamic created textfield
    add fxid to the prebuild textfields

--- src/main/java/com/mn210511/specialbonuscalculator/BonusCalculatorController.java
+++ src/main/java/com/mn210511/specialbonuscalculator/BonusCalculatorController.java
@@ -2,6 +2,7 @@ package com.mn210511.specialbonuscalculator;
 
 import com.mn210511.specialbonuscalculator.services.Calculator;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 
 
@@ -10,6 +11,9 @@ import javafx.scene.control.Label;
 public class BonusCalculatorController {
     @FXML
     private Label welcomeText;
+
+    private Node[] hourFields = new Node[12];
+    private Node[] dayFields = new Node[12];
 
 
     public void initialize() {
@@ -27,6 +31,8 @@ public class BonusCalculatorController {
     @FXML
     protected  void onPlusButtonClick() {
         System.out.println("plus geklickt");
+
+
     }
 
 
