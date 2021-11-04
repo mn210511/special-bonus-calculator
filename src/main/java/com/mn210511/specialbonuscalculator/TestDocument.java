f5f9c0c163974625d6837be17b0edc38e836bb31
mn210511 <nicolas.herold@outlook.at>
2021-11-04 19:19:58 +0100

    update the class to create the PDF

--- src/main/java/com/mn210511/specialbonuscalculator/TestDocument.java
+++ src/main/java/com/mn210511/specialbonuscalculator/TestDocument.java
@@ -8,6 +8,7 @@ import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.state.RenderingMode;
 
 import java.io.File;
 import java.io.IOException;
@@ -22,14 +23,7 @@ public class TestDocument {
         document.addPage(page);
         try {
             PDPageContentStream stream = new PDPageContentStream(document, page);
-            stream.beginText();
-            stream.setFont(PDType1Font.TIMES_ROMAN, 10);
-            stream.newLineAtOffset(0, 25);
 
-            stream.showText("TESt /n TEst");
-
-
-            stream.endText();
 
             float xStart = mediabox.getLowerLeftX();
             float yStart = mediabox.getLowerLeftY();
@@ -37,30 +31,57 @@ public class TestDocument {
             float xEnd = mediabox.getUpperRightX();
             float yEnd = mediabox.getUpperRightY();
 
-            int rows = 14;
+            int rows = 15;
             float gap = 20.0f;
 
             //drawing the horizontal lines
             for (int i = 0; i < rows; i++) {
                 xStart = 100.0f;
                 xEnd = xStart + 400.0f;
-                yStart = 500.0f;
-                yEnd = 500.0f;
+                yStart = 600.0f;
+                yEnd = 600.0f;
 
                 drawLine(stream, xStart, yStart - (gap * i), xEnd, yEnd - (gap * i));
 
 
             }
+            stream.beginText();
+            stream.setFont(PDType1Font.TIMES_ROMAN, 20);
+            stream.newLineAtOffset(20, mediabox.getUpperRightY()-50);
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
+            stream.showText("Firma: ");
+           // stream.newLineAtOffset(20, mediabox.getUpperRightY()-200);
+            stream.newLineAtOffset(0, -20);
+            stream.showText("Mitarbeiter: ");
+           stream.setRenderingMode(RenderingMode.FILL);
+            stream.setFont(PDType1Font.TIMES_ROMAN, 18);
+            stream.newLineAtOffset(100, 0);
+            stream.showText("hier wird ein Name stehen");
+            stream.newLineAtOffset(0, +20);
+            stream.showText("hier wird ein Klient stehen");
+    stream.endText();
+
 
             //drawing the vertical lines
             int columns = 5;
-            xEnd=xStart;
-            yEnd= yStart-(rows*gap)+gap;
+            xEnd = xStart;
+            yEnd = yStart - (rows * gap) + gap;
             gap = 100.0f;
-            for (int i = 0; i < columns; i++){
+            for (int i = 0; i < columns; i++) {
 
-                drawLine(stream, xStart+ (gap * i), yStart, xEnd+ (gap * i), yEnd);
+                drawLine(stream, xStart + (gap * i), yStart, xEnd + (gap * i), yEnd);
             }
+
+            //longer lines on the outside border
+            drawLine(stream, xStart, yStart, xStart, yEnd - 105);
+            drawLine(stream, xStart + (gap * columns) - gap, yStart, xStart + (gap * columns) - gap, yEnd - 105);
+
+            //line between summed AVG and the endresult
+            drawLine(stream, xStart, yEnd-50, xEnd+(columns*gap)-gap, yEnd-50);
+
+            //line for the bottom border
+            drawLine(stream, xStart, yEnd-100, xEnd+(columns*gap)-gap, yEnd-100);
+            drawLine(stream, xStart, yEnd-105, xEnd+(columns*gap)-gap, yEnd-105);
 
 
             System.out.println("xstart: " + xStart + "yStart" + yStart);
@@ -89,6 +110,7 @@ public class TestDocument {
     private static void drawLine(PDPageContentStream stream, float xStart, float yStart, float xEnd, float yEnd) throws IOException {
         stream.moveTo(xStart, yStart);
         stream.lineTo(xEnd, yEnd);
-        stream.stroke();
+        stream.fillAndStroke();
     }
+
 }
