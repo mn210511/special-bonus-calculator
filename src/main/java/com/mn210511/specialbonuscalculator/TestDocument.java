f45cf514a75555245b318ca85776380e783fc04b
mn210511 <nicolas.herold@outlook.at>
2021-11-07 16:37:24 +0100

    update pdfcreation
    add Headlines for the tables
    add bottom line with the creation date
    add fields for the bonus and average values

--- src/main/java/com/mn210511/specialbonuscalculator/TestDocument.java
+++ src/main/java/com/mn210511/specialbonuscalculator/TestDocument.java
@@ -1,6 +1,7 @@
 package com.mn210511.specialbonuscalculator;
 
 import com.mn210511.specialbonuscalculator.entities.Record;
+import com.mn210511.specialbonuscalculator.entities.Worktime;
 import javafx.stage.FileChooser;
 import javafx.stage.PopupWindow;
 import javafx.stage.Window;
@@ -13,13 +14,30 @@ import org.apache.pdfbox.pdmodel.graphics.state.RenderingMode;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.FormatStyle;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TestDocument {
 
-   ;
+    ;
 
     public static void main(String[] args) {
         Record testRecord = new Record("MyCompany", "Nicolas Herold", true, 2500.0);
+        List<Worktime> worktimes = new ArrayList<>();
+        for (int i = 0; i < 12; i++) {
+            Worktime tmp = new Worktime(20.0 + i, i);
+            tmp.setAverage(2000);
+            worktimes.add(tmp);
+
+
+        }
+        testRecord.setWorktimes(worktimes);
         PDDocument document = new PDDocument();
         PDPage page = new PDPage();
 
@@ -51,20 +69,77 @@ public class TestDocument {
             }
             stream.beginText();
             stream.setFont(PDType1Font.TIMES_ROMAN, 20);
-            stream.newLineAtOffset(20, mediabox.getUpperRightY()-50);
+            stream.newLineAtOffset(20, mediabox.getUpperRightY() - 50);
             stream.setRenderingMode(RenderingMode.FILL_STROKE);
             stream.showText("Firma: ");
-           // stream.newLineAtOffset(20, mediabox.getUpperRightY()-200);
+            // stream.newLineAtOffset(20, mediabox.getUpperRightY()-200);
             stream.newLineAtOffset(0, -20);
             stream.showText("Mitarbeiter: ");
-           stream.setRenderingMode(RenderingMode.FILL);
+            stream.setRenderingMode(RenderingMode.FILL);
             stream.setFont(PDType1Font.TIMES_ROMAN, 18);
             stream.newLineAtOffset(100, 0);
             stream.showText(testRecord.getEmployee());
             stream.newLineAtOffset(0, +20);
             stream.showText(testRecord.getCompany());
             stream.endText();
+            float lineOffset = 210.0f;
 
+            //Headlines
+            stream.beginText();
+            stream.newLineAtOffset(110, mediabox.getUpperRightY()-lineOffset);
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
+            stream.showText("Datum");
+            stream.newLineAtOffset(100, 0);
+            stream.showText("Stunden");
+            stream.newLineAtOffset(100, 0);
+            stream.showText("Tage");
+            stream.newLineAtOffset(100, 0);
+            stream.showText("Schnitt");
+            stream.endText();
+            lineOffset = 230.0f;
+
+            // values of the Calculation inside the cells
+            for (Worktime w : testRecord.getWorktimes()) {
+                stream.setFont(PDType1Font.COURIER, 15);
+                stream.setRenderingMode(RenderingMode.FILL);
+                stream.beginText();
+                stream.newLineAtOffset(110, mediabox.getUpperRightY()-lineOffset);
+                stream.showText(String.valueOf(w.getDuration()));
+                stream.newLineAtOffset(100, 0);
+                stream.showText(String.valueOf(w.getHoursPerWeek()));
+                stream.newLineAtOffset(100, 0);
+                stream.showText((String.valueOf(w.getAverage())));
+                stream.endText();
+                lineOffset -= -20;
+            }
+
+            //Bottom line of the table
+            stream.beginText();
+            stream.newLineAtOffset(310, mediabox.getUpperRightY()-lineOffset);
+            stream.showText("366");
+            stream.newLineAtOffset(100, 0);
+            stream.showText(String.valueOf(testRecord.getAverage()));
+
+            stream.endText();
+
+            lineOffset -= -50;
+            //Line for the average
+            stream.beginText();
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
+            stream.newLineAtOffset(110, mediabox.getUpperRightY()-lineOffset);
+            stream.showText("Schnitt:");
+            stream.newLineAtOffset(300, 0);
+            stream.showText("90000");
+            stream.endText();
+
+            //line for the endresult
+            lineOffset -= -50;
+            stream.beginText();
+            stream.newLineAtOffset(110, mediabox.getUpperRightY()-lineOffset);
+            stream.showText("Sonderzahlung gesamt:");
+            stream.newLineAtOffset(300, 0);
+            stream.showText(String.valueOf(90000));
+            stream.endText();
 
             //drawing the vertical lines
             int columns = 5;
@@ -81,11 +156,20 @@ public class TestDocument {
             drawLine(stream, xStart + (gap * columns) - gap, yStart, xStart + (gap * columns) - gap, yEnd - 105);
 
             //line between summed AVG and the endresult
-            drawLine(stream, xStart, yEnd-50, xEnd+(columns*gap)-gap, yEnd-50);
+            drawLine(stream, xStart, yEnd - 50, xEnd + (columns * gap) - gap, yEnd - 50);
 
             //line for the bottom border
-            drawLine(stream, xStart, yEnd-100, xEnd+(columns*gap)-gap, yEnd-100);
-            drawLine(stream, xStart, yEnd-105, xEnd+(columns*gap)-gap, yEnd-105);
+            drawLine(stream, xStart, yEnd - 100, xEnd + (columns * gap) - gap, yEnd - 100);
+            drawLine(stream, xStart, yEnd - 105, xEnd + (columns * gap) - gap, yEnd - 105);
+
+// Line with the creation Date at the bottom
+            stream.beginText();
+            stream.setRenderingMode(RenderingMode.FILL);
+            stream.setFont(PDType1Font.TIMES_ROMAN, 15);
+            stream.newLineAtOffset(mediabox.getUpperRightX()-150, mediabox.getLowerLeftY()+20);
+
+            stream.showText("Erstellt am " + DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM).format(LocalDate.now()));
+            stream.endText();
 
 
             System.out.println("xstart: " + xStart + "yStart" + yStart);
@@ -94,7 +178,7 @@ public class TestDocument {
             stream.close();
 
 
-        } catch (IOException e) {
+        } catch (Exception e) {
             System.err.println(e.getMessage());
         }
 
