ea5ff6754c5664bf1b05aaac167851c9c109d19b
mn210511 <nicolas.herold@outlook.at>
2021-11-07 17:06:05 +0100

    update to the worktime class
    add attributes to store the values of
    the start and end of the worktimemodell

--- src/main/java/com/mn210511/specialbonuscalculator/entities/Worktime.java
+++ src/main/java/com/mn210511/specialbonuscalculator/entities/Worktime.java
@@ -1,5 +1,7 @@
 package com.mn210511.specialbonuscalculator.entities;
 
+import java.time.LocalDate;
+
 /**
  * Class to store a Worktime data
  */
@@ -7,10 +9,35 @@ public class Worktime {
     double hoursPerWeek;
     int duration;
     double average;
+    LocalDate begin;
+    LocalDate end;
 
     public Worktime(double hoursPerWeek, int duration) {
         this.hoursPerWeek = hoursPerWeek;
         this.duration = duration;
+    }
+
+    public Worktime(double hoursPerWeek, int duration, LocalDate begin, LocalDate end) {
+        this.hoursPerWeek = hoursPerWeek;
+        this.duration = duration;
+        this.begin = begin;
+        this.end = end;
+    }
+
+    public LocalDate getBegin() {
+        return begin;
+    }
+
+    public void setBegin(LocalDate begin) {
+        this.begin = begin;
+    }
+
+    public LocalDate getEnd() {
+        return end;
+    }
+
+    public void setEnd(LocalDate end) {
+        this.end = end;
     }
 
     public double getAverage() {
