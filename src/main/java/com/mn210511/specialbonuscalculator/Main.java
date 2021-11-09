6f5851077a96dd0db1129fb8672640f2236c5ff5
mn210511 <nicolas.herold@outlook.at>
2021-11-09 18:23:17 +0100

    create extra GUI
    update the GUI for 3-Month average calculation modell

--- src/main/java/com/mn210511/specialbonuscalculator/Main.java
+++ src/main/java/com/mn210511/specialbonuscalculator/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("bonus-calculator.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
         stage.setTitle("special-bonus Calculator");
         stage.setScene(scene);
         stage.show();
